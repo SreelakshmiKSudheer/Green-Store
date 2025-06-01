9e3fc4ae35159ea6268b1e9b5650929a41a899ba
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-06-01 23:23:19 +0530

    Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useLocation } from 'react-router'
 
 const Product = () => {
+  const location = useLocation()
+  const { product } = location.state || {};
+
+  if (!product) {
+    return (
+      <div className='w-full h-full flex flex-col items-center justify-center p-5 m-5  bg-white shadow-lg rounded-lg'>
+        <h2 className='text-2xl font-bold'>Product not found</h2>
+      </div>
+    )
+  }
   return (
-    <div>Product</div>
+    <div className='w-full h-full flex flex-col items-start justify-start p-5 m-5 bg-white shadow-lg rounded-lg'>
+      <img src={product.image} alt={product.title}  className='w-auto h-100 object-fit mx-auto rounded-lg mb-4'/>
+      <h2 className='text-2xl font-bold'>{product.title}</h2>
+      <p>{product.description}</p>
+      <p>Category: {product.category}</p>
+      <p>Price: ${product.price}</p>
+    </div>
   )
 }
 
